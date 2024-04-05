0424c74b2ffebaa17923dd253ac7457a57b83574
Raccffy <165374321+Raccffy@users.noreply.github.com>
2024-04-05 23:19:14 +0500

    Add Ogg obfuscated resource pack extraction test.
    This adds Ogg obfuscated resource pack extraction test with a
    reference.
    
    Sound files can be corrupted by removing Vorbis' magic number from
    the stream. Surprisingly, newer game versions are decoding and
    playing these files without any errors.
    
    Note that Ogg Vorbis autocorrection is currently not implemented.
    
    Compiled resource pack was generated with PackSquash's
    2024-04-04 [8ea2917] build.

--- tests.py
+++ tests.py
@@ -62,5 +62,31 @@ class Test_A_ResourcePackExtraction(unittest.TestCase):
         extracffy.close()
 
 
+class Test_B_OggDeobfs(unittest.TestCase):
+    def test_01_ref(self):
+        extracffy = Extracffy("tests/ref_ogg_obfs.zip")
+        extracffy_idx = extracffy.cd_read()
+
+        for idx in extracffy_idx:
+            extracffy.extract(idx, True)
+
+        extracffy.close()
+
+    def test_02_deobfs(self):
+        """
+        Currently there's no Vorbis autocorrection in Extracffy. Ogg files
+        are not readable due to corrupted codec magic number, which leads
+        to the "End of file" error.
+        """
+
+        extracffy = Extracffy("tests/ps_20240404_8ea2917_ogg_obfs.zip")
+        extracffy_idx = extracffy.cd_read()
+
+        for idx in extracffy_idx:
+            extracffy.extract(idx, True)
+
+        extracffy.close()
+
+
 if __name__ == '__main__':
     unittest.main()
